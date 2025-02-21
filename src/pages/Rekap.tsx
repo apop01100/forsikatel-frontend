d89c1cc5e8522e4c6d4daab85e098b66e5f0cae0
rifqihananto <127004043+Killerz1724@users.noreply.github.com>
2025-02-21 14:23:40 +0700

    Changes

--- src/pages/Rekap.tsx
+++ src/pages/Rekap.tsx
@@ -1,16 +1,17 @@
-// import ComingSoon from "../components/ComingSoon"
-import TotalKhatamJuzCarousel from "../components/TotalKhatamJuzCarousel"
-import DetailPeroranganTable from "../components/DetailPeroranganTable"
+import ComingSoon from "../components/ComingSoon"
+// import TotalKhatamJuzCarousel from "../components/TotalKhatamJuzCarousel"
+// import DetailPeroranganTable from "../components/DetailPeroranganTable"
 const Rekap = () => {
     return (
-      <section className="flex flex-col items-center w-full gap-[2rem]">
-        <div className="flex items-center gap-5">
-            <TotalKhatamJuzCarousel />
-        </div>
-        <div className="w-full">
-            <DetailPeroranganTable />
-        </div>
-      </section>
+      // <section className="flex flex-col items-center w-full gap-[2rem]">
+      //   <div className="flex items-center gap-5">
+      //       <TotalKhatamJuzCarousel />
+      //   </div>
+      //   <div className="w-full">
+      //       <DetailPeroranganTable />
+      //   </div>
+      // </section>
+      <ComingSoon />
     )
   }
   
