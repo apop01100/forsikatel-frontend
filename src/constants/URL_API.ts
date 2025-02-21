d8ae5950b0af1ea7e1a76f8614ad6bb4c853cc36
rifqihananto <127004043+Killerz1724@users.noreply.github.com>
2025-02-21 13:59:42 +0700

    changes

--- src/constants/URL_API.ts
+++ src/constants/URL_API.ts
@@ -1,7 +1,9 @@
 const API_URL = import.meta.env.VITE_FORSIKATEL_API_URL
 export const API_REGISTER = `${API_URL}/users/register`
 export const API_LOGIN = `${API_URL}/users/login`
+export const API_AUTH = `${API_URL}/users/authenticate`
 export const API_HADIST_KULTUM = `${API_URL}/hadist-kultum/`
 export const API_SETOR_NGAJI = `${API_URL}/setoran-ngaji/`
 export const API_DASHBOARD = `${API_URL}/dashboard/`
 export const API_PROGRESS_CHART = `${API_URL}/setoran-ngaji/chart_ngaji`
+
