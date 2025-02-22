4622cc8d74d46ba9b63ea922da774238d04e0811
Thoriq Afif <mthoriqafif01@gmail.com>
2025-02-22 15:32:15 +0700

    refactor: footer

--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -2,10 +2,7 @@ import HeaderLogo from "./HeaderLogo"
 
 const HeaderContent = () => {
   return (
-    <div className="flex items-center gap-2 px-2 pt-2 w-full justify-between">
-        <span className="text-sm font-normal text-neutral-300">
-            Copyright ©2025 All Rights Reserved | Forsikatel Group Telkom Akses - KTI
-        </span>
+    <div className="flex items-center gap-2 px-4 pt-2 w-full justify-end">
         <HeaderLogo imgSize="w-16 h-8" classImg="object-contain" />
     </div>
   )
