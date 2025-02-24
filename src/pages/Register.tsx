74b938f6a12b1c644b92170813ae2dbb19fa8f30
Thoriq Afif <mthoriqafif01@gmail.com>
2025-02-24 17:17:20 +0700

    feature: progress page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,12 +3,10 @@ import Footer from "../components/Footer"
 import { RegisterPage } from "../components/RegisterPage"
 const Register = () => {
     return (
-      <>
         <div className="w-full gap-2 flex flex-col justify-between">
             <RegisterPage />
             <Footer />
         </div>
-      </>
     )
   }
   
